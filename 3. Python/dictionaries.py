609148d1478d00937dcad1d21ebb8a0da979570e
mgrybel <monika.grybel@hotmail.com>
2021-07-09 21:35:09 +0100

    Update

---
+++ 3. Python/dictionaries.py
@@ -0,0 +1,9 @@
+# Create a dictionary
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+print(houses["Harry"])
+
+# Add a new element to the dictionary
+houses["Hermione"] = "Gryffindor"
+
+print(houses)
